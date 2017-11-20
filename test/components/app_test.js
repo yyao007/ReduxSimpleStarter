3872f4548aaf2a814b710da3904d5245595507f6
Yuan Yao <yaoyuan526@gmail.com>
2017-11-19 22:17:58 -0800

    fixed test_helper.js

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,15 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// Use describe to group together similar tests
+describe('App', () => {
+	// Use it to test a single attribute of a target
+	it('shows the correct text', () => {
+		// create an instance of App
+		const component = renderComponent(App);
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
+		// Use expect to make an assertion about a target
+		expect(component).to.contain('React simple starter');
+	});
 });
+
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,16 +1,17 @@
 import _$ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
+import TestUtils from 'react-dom/test-utils';
+import { JSDOM } from 'jsdom';
 import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
+global.dom = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = global.dom.window;
+global.document = global.window.document;
 global.navigator = global.window.navigator;
 const $ = _$(window);
 
